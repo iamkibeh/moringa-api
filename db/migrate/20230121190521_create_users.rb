e407ae3e1fc346f460274dd74277e66b0e50598e
DenisOuma <dennisouma38@gmail.com>
2023-01-21 22:07:17 +0300

    generating a model for user

--- db/migrate/20230121190521_create_users.rb
+++ db/migrate/20230121190521_create_users.rb
@@ -1,10 +1,10 @@
-class CreateFirstNames < ActiveRecord::Migration[7.0]
+class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :first_names do |t|
+    create_table :users do |t|
+      t.string :first_name
       t.string :last_name
       t.string :email
       t.string :phone_number
-      t.string :email
       t.string :password_digest
       t.boolean :agreement
       t.string :github_url
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_21_190521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.string "password_digest"
+    t.boolean "agreement"
+    t.string "github_url"
+    t.string "linkedin_url"
+    t.string "twitter_url"
+    t.string "cv_url"
+    t.string "profile_img"
+    t.string "cover_img"
+    t.string "country"
+    t.string "bio"
+    t.string "profession"
+    t.string "user_type"
+    t.string "company_name"
+    t.string "company_website"
+    t.string "user_location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
