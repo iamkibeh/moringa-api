7ec85ff303e151ea2611ca35d2110c6949e6a2ac
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-01-24 22:49:12 +0300

    add skip before authorize for create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :email, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create } 
     validates :first_name, presence: true
     validates :last_name, presence: true
     # validates :password, length: {minimum: 4, maximum: 20}
