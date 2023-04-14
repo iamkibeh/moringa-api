9e62d9e9e34fb46e42779a0ed03de35ff6683692
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-04-14 18:23:51 +0300

    add routes for comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :comments
+  # resources :likes
+  # resources :comments
   resources :posts
   # resource :users, only: %i[update destroy]
   post "/login", to: "auth#create"
@@ -8,4 +8,19 @@ Rails.application.routes.draw do
   get "/users", to: "users#index"
   get "/users/:id", to: "users#show"
   patch "/users/:id", to: "users#update"
+  get "users/:id/activities", to: "users#activities"
+
+  # post related routes
+  # get "/posts", to: "posts#index"
+
+  # likes related routes
+  get "/posts/:id/likes", to: "likes#index"
+  post "/posts/:id/likes", to: "likes#create"
+  delete "/posts/:id/likes", to: "likes#destroy"
+
+  # comments related routes
+  get "/posts/:id/comments", to: "comments#index"
+  post "/posts/:id/comments", to: "comments#create"
+  patch "/posts/:id/comments", to: "comments#update"
+  delete "/posts/:id/comments", to: "comments#destroy"
 end
