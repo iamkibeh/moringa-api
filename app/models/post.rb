c9eb0b2e281184d0c3d13d48055572798b88accb
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-04-14 18:22:27 +0300

    create post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  # associations with other models
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  # validations for post model
+  validates :user_id, presence: true
+  validate :at_least_one_attribute_present
+
+  def at_least_one_attribute_present
+    # if [title, content].all?(&:blank?)
+    unless post_description.present? || post_img.present? || post_title.present?
+      errors.add(:base, "Post must have a description, title or image.")
+    end
+  end
 end
