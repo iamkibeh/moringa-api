3da6a49cb64f96b4958e4084ac690b93e83a446c
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-04-14 13:04:26 +0300

    create post model and the necessary controller

---
+++ db/migrate/20230414020119_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :post_title
+      t.string :post_description
+      t.string :post_img
+      t.integer :post_like
+      t.string :post_category
+      t.string :post_comment
+      t.string :post_type
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
