0ba5377df16225e2ed91fbaef2fbbd3405cb2e31
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-05-13 23:11:10 +0300

    resolve the user serializer bug when  displaying replies

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,5 +4,5 @@ class Comment < ApplicationRecord
   belongs_to :post
   #   a comment can have many comments as children ie replies (parent_comment_id)
   belongs_to :parent_comment, class_name: "Comment", optional: true
-  has_many :replies, class_name: "Comment", foreign_key: "parent_comment_id"
+  has_many :replies, class_name: "Comment", foreign_key: "parent_comment_id", dependent: :destroy
 end
